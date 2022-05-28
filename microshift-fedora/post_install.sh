1c2c32cfe52ab93254c1fadb69b9d25c4327ee6d
Administrator <admin@example.com>
2022-05-28 16:17:51 +0800

    Update post install script

--- microshift-fedora/post_install.sh
+++ microshift-fedora/post_install.sh
@@ -3,6 +3,11 @@ curl -O https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/st
 sudo tar -xf openshift-client-linux.tar.gz -C /usr/local/bin oc kubectl
 
 # Copy Kubeconfig
-mkdir ~/.kube
+mkdir -p ~/.kube
 sudo podman cp microshift:/var/lib/microshift/resources/kubeadmin/kubeconfig ~/.kube/config
 sudo chown `whoami`: ~/.kube/config
+
+# OC bash completion
+sudo dnf -y install bash-completion
+oc completion bash > oc_bash_completion
+sudo cp oc_bash_completion /etc/bash_completion.d/
